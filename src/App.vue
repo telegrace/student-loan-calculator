a0b8bff6e157ae35000982091ef8803859d8073b
telegrace <telegrace@gmx.de>
2022-12-26 14:04:15 +0100

    init

---
+++ src/App.vue
@@ -0,0 +1,4 @@
+<template><HomePageComponent /></template>
+<script setup lang="ts">
+import HomePageComponent from "./pages/views/home/HomePageComponent.vue";
+</script>
---
+++ src/pages/views/home/HomePageComponent.vue
@@ -0,0 +1,1 @@
+<template>Student Loan Calculator</template>
